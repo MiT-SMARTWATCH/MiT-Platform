88a5fe4191c6d8d35d8784f322a53d3bf752d4d9
MiT-SMARTWATCH <85783150+MiT-SMARTWATCH@users.noreply.github.com>
2021-06-13 04:18:12 +0900

    Create device_maker.py

---
+++ server_programming/device_maker.py
@@ -0,0 +1,70 @@
+import pandas as pd
+from pandas._libs.tslibs import NullFrequencyError
+import psycopg2
+from sqlalchemy import create_engine
+import logging
+# Importing models and REST client class from Community Edition version
+from tb_rest_client.rest_client_ce import *
+from tb_rest_client.rest import ApiException
+
+password = 7452
+
+def createEngineUser_info(password):
+    engine_user_info = create_engine(f'postgresql://postgres:{password}@127.0.0.1:5432/user_info')
+    return engine_user_info
+
+def createEngineThingsboard(password):
+    engine_Thingsboard = create_engine(f'postgresql://postgres:{password}@127.0.0.1:5432/thingsboard')
+    return engine_Thingsboard
+
+################# device
+def MakeDeviceForRegister(device, user_ID, password):
+    engine_user_info = createEngineUser_info(password)
+
+    # 예외처리 필요
+    # user_ID = pd.read_sql_query(sql=f"SELECT user_id FROM user_device WHERE user_id = '{user_ID}'", con=engine_user_info)
+    # user_ID = pd.read_sql_query(sql=f"SELECT 이름 FROM user_device WHERE 아이디 = '{user_ID}'", con=engine_user_info)
+
+    device_name = user_ID + "_smartwatch"
+
+    # Thingsboard API
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s - %(module)s - %(lineno)d - %(message)s',
+                        datefmt='%Y-%m-%d %H:%M:%S')
+
+    # ThingsBoard URL
+    url = "http://localhost:8080"
+
+    # Default Tenant Administrator credentials
+    username = "tenant@thingsboard.org"
+    password = "tenant"
+
+    # Creating the REST client object with context manager to get auto token refresh
+    with RestClientCE(base_url=url) as rest_client:
+        try:
+            # Auth with credentials
+            rest_client.login(username=username, password=password)
+
+            # creating a Device
+            device = Device(name=device_name, type="thermometer")
+            device = rest_client.save_device(device)
+
+            logging.info(" Device was created:\n%r\n", device)
+        except ApiException as e:
+            logging.exception(e)
+
+    engine_Thingsboard = createEngineThingsboard(password)
+
+    # device_id =  pd.read_sql_query(sql = f"SELECT id FROM device WHERE name = '{device_name}'",con=engine_Thingsboard)
+    device_id =  pd.read_sql_query(sql = f"SELECT id FROM device WHERE name = '{device_name}'",con=engine_Thingsboard)
+    device_id = device_id["id"][0]
+    print(device_id)
+
+    token_key = pd.read_sql_query(sql = f"SELECT credentials_id FROM device_credentials WHERE device_id = '{device_id}'",con=engine_Thingsboard)
+    token_key = token_key["credentials_id"][0]
+    print(token_key)
+
+    engine_user_info.execute(f"UPDATE user_device SET device_id = '{device_id}', token_key = '{token_key}' Where user_id = '{user_ID}';")
+
+    engine_Thingsboard.dispose()
+    engine_user_info.dispose()
